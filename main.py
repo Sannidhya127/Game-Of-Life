39c27878fdd5229c48f14af1dbd9f46043ec89c6
Sannidhya <shubhradasgupta8@gmail.com>
2022-05-01 12:33:52 +0530

    Added color codes and the uodate func params

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+import pygame 
+import numpy as np
+
+COLOR_BG = (10,10,10)
+
+COLOR_GRID = (40,40,40)
+
+COLOR_DIE_NEXT = (170,170,170)
+
+COLOR_ALIVE = (255, 255, 255)
+
+def update(screen, cells, size, with_progress=False):
+    pass
